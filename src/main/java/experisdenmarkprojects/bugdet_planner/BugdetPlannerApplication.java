a64602a670731492b87dfe0f51d10f3abf11e656
Alexander Jonstrup <Alexander.Jonstrup@outlook.com>
2023-11-06 00:43:01 +0100

    Initialized project.

---
+++ src/main/java/experisdenmarkprojects/bugdet_planner/BugdetPlannerApplication.java
@@ -0,0 +1,13 @@
+package experisdenmarkprojects.bugdet_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugdetPlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BugdetPlannerApplication.class, args);
+    }
+
+}
